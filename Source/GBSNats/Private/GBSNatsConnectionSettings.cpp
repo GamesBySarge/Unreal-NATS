12c335a5cae4a1d1199afc57bc6940af531bf4b7
Tony Richards <trichards@indiezen.com>
2022-03-25 16:57:17 -0400

    Added SkipServerVerification and SetExpectedHostName

--- Source/GBSNats/Private/GBSNatsConnectionSettings.cpp
+++ Source/GBSNats/Private/GBSNatsConnectionSettings.cpp
@@ -3,6 +3,8 @@
 UGBSNatsConnectionSettings::UGBSNatsConnectionSettings(const class FObjectInitializer& PCIP)
 	: Super(PCIP)
 {
+  // TODO this is being leaked; destroy it later, but when?
+  natsOptions_Create(&this->opts);
 }
 
 void UGBSNatsConnectionSettings::AddServer(const FString& Server)
@@ -10,9 +12,25 @@ void UGBSNatsConnectionSettings::AddServer(const FString& Server)
   Servers.Add(Server);
 }
 
+void UGBSNatsConnectionSettings::SetExpectedHostName(const FString& HostName)
+{
+  // TODO Handle errors
+  natsOptions_SetExpectedHostname(this->opts, TCHAR_TO_ANSI(*HostName));
+}
+
+void UGBSNatsConnectionSettings::SkipServerVerification(const bool Skip)
+{
+  // TODO Handle errors
+  natsOptions_SkipServerVerification(this->opts, Skip);
+}
 
 FString UGBSNatsConnectionSettings::GetConnectionString()
 {
   FString connectionString = FString::Join(this->Servers, TEXT(", "));
   return connectionString;
+}
+
+natsOptions* UGBSNatsConnectionSettings::GetOptions()
+{
+  return this->opts;
 }
--- Source/GBSNats/Public/GBSNatsConnectionSettings.h
+++ Source/GBSNats/Public/GBSNatsConnectionSettings.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "nats/nats.h"
+
 #include "GBSNatsConnectionSettings.generated.h"
 
 UCLASS(BlueprintType, Blueprintable)
@@ -17,12 +19,28 @@ public:
   UFUNCTION(BlueprintAuthorityOnly, BlueprintCallable, Category = "GBSNats|Connection")
   void AddServer(const FString& Server);
 
+  /**
+   * Sets the expected host name
+   * 
+   * Use this if the server name to which you're connecting is different from
+   * that server's internal host name.
+   */
+  UFUNCTION(BlueprintAuthorityOnly, BlueprintCallable, Category = "GBSNats|Connection")
+  void SetExpectedHostName(const FString& HostName);
+
+  /**
+   * Skip the TLS verification
+   */
+  UFUNCTION(BlueprintAuthorityOnly, BlueprintCallable, Category = "GBSNats|Connection")
+  void SkipServerVerification(const bool Skip);
+
   /////////////////////////////////////////////////////
   // Internal C++ stuff, not used by blueprints
 public:
   FString GetConnectionString();
+  natsOptions* GetOptions();
 
 private:
   TArray<FString> Servers;
+  natsOptions* opts;
 };
-
